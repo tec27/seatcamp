30ea0115e411064970e7d78840dc84a1097a4e94
Travis Collins <travis@tec27.com>
2014-10-28 22:45:12 -0700

    Move to using a json config instead of flags. Flags would be getting out of control for dev setups shortly.

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ var express = require('express')
   , serveStatic = require('serve-static')
   , userCounter = require('./lib/user-counter')
   , chatSockets = require('./lib/chat-sockets')
+  , config = require('./conf.json')
 
-var userIdKey = process.env.ID_KEY
-if (!userIdKey && process.env.NODE_ENV == 'production') {
-  throw new Error('ID_KEY must be specified in production')
-} else {
-  userIdKey = 'seatcamp'
+var userIdKey = config.idKey
+if (!userIdKey) {
+  throw new Error('idKey must be specified in conf.json!')
 }
 
 var app = express()
@@ -29,7 +28,7 @@ app.use(serveStatic('public'))
 userCounter(io)
 chatSockets(io, userIdKey, 15 /* server backscroll limit */, 10 * 60 * 1000 /* expiry time */)
 
-httpServer.listen(process.env.PORT || 3456, function() {
+httpServer.listen(config.port, function() {
   var host = httpServer.address().address
     , port = httpServer.address().port
   console.log('Listening at http://%s:%s', host, port)
