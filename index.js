9b9ce8ac83440ab88016b22c744fa48d38ad8644
Travis Collins <travis@tec27.com>
2015-03-15 13:00:10 -0700

    Detect iojs and disable unnecessary babel transforms.

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 // TODO(tec27): detect if in iojs and disable options that transpile things supported natively
+function isIojs() {
+  try {
+    eval("(function*(){})");
+    return true;
+  } catch(err) {
+    return false;
+  }
+}
+
+var blacklist = !isIojs() ? [] : [
+  'es6.blockScoping',
+  'es6.constants',
+  'es6.templateLiterals',
+  'regenerator',
+]
+
 require('babel/register', {
   loose: 'all',
+  blacklist: blacklist
 })
 
 require('./server')
