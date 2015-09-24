05c6bfe300f78d087d78712f2407a826673647f6
Travis Collins <travis@tec27.com>
2015-09-24 16:36:34 -0700

    Move to only supporting node >4, use babelrc for babel config (and update for node v4 es6 support).

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-function isIojs() {
-  try {
-    eval("(function*(){})");
-    return true;
-  } catch(err) {
-    return false;
-  }
-}
-
-var blacklist = !isIojs() ? [] : [
-  'es6.blockScoping',
-  'es6.constants',
-  'es6.templateLiterals',
-  'regenerator',
-]
-
-require('babel/register', {
-  loose: 'all',
-  blacklist: blacklist
-})
-
+require('babel/register')
 require('./server')
