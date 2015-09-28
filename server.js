fe385f8b1965c92612cf8dc4501443420c090faa
Travis Collins <travis@tec27.com>
2015-09-27 17:48:24 -0700

    Use __dirname instead of just straight CWD relative paths. Fixes #10.

--- server.js
+++ server.js
@@ -84,8 +84,8 @@ if (process.env.NODE_ENV === 'production') {
 
 app
   .get('/', (req, res) => res.render('index', { theme: req.cookies.theme }))
-  .get('/client.js', browserify('./client/index.js', browserifyOpts))
-  .get('/styles.css', serveCss('./css/styles.css'))
+  .get('/client.js', browserify(__dirname + '/client/index.js', browserifyOpts))
+  .get('/styles.css', serveCss(__dirname + './css/styles.css'))
 
 app.use(serveStatic('public'))
 
